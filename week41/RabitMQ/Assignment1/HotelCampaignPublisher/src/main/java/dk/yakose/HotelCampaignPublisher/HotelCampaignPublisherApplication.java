0d8922d861dfb2af6aa4b427265c0b230255d72d
Yakubu Adeyemi Oseni <yakose@users.noreply.github.com>
2019-10-20 18:21:26 +0200

    Add files via upload

---
+++ week41/RabitMQ/Assignment1/HotelCampaignPublisher/src/main/java/dk/yakose/HotelCampaignPublisher/HotelCampaignPublisherApplication.java
@@ -0,0 +1,13 @@
+package dk.yakose.HotelCampaignPublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCampaignPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelCampaignPublisherApplication.class, args);
+	}
+
+}
